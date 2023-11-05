686785d2b05b89d0a3825ce528d679a1bb570bdd
Rayumar89 <rayumarj@gmail.com>
2023-11-04 22:55:44 -0700

    jvs4.1

---
+++ app.js
@@ -0,0 +1,57 @@
+var num = 5
+document.write("Initial value:" + num +"<br>")
+num++;
+document.write("value after increment is :" + num + "<br>")
+
+num = num + 7
+document.write("value after addition is :" + num + "<br>")
+
+num--;
+document.write("value after decrement is :" + num + "<br>")
+
+num = num % 3
+document.write("the remainder is :" + num + "<br>" + "<br>" + "<br>")
+
+
+
+// tickets assingmint
+
+var cost = 600
+var x = prompt("enter your numer of tickets")
+document.write("total cost of buy" + " " + x + " " + "tickets is :" + " " + x * cost + "<br>" + "<br>" + "<br>")
+
+
+// table
+
+
+var v = prompt("enter the number for table")
+
+document.write(v + "x" + " " + 1 + "="+ v*1 + "<br>")
+document.write(v + "x" + " " + 2 + "="+ v*2 + "<br>")
+document.write(v + "x" + " " + 3 + "="+ v*3 + "<br>")
+document.write(v + "x" + " " + 4 + "="+ v*4 + "<br>")
+document.write(v + "x" + " " + 5 + "="+ v*5 + "<br>")
+document.write(v + "x" + " " + 6 + "="+ v*6 + "<br>")
+document.write(v + "x" + " " + 7 + "="+ v*7 + "<br>")
+document.write(v + "x" + " " + 8 + "="+ v*8 + "<br>")
+document.write(v + "x" + " " + 9 + "="+ v*9 + "<br>")
+document.write(v + "x" + " " + 10 + "="+ v*10 + "<br>" + "<br>"+ "<br>"+ "<br>")
+
+
+
+var p = prompt("price of item 1")
+var q = prompt("enter the quantity of item 1")
+var r = prompt("price of item 2")
+var s = prompt("enter the quantity of item 2")
+var t = 100
+
+document.write("price of item 1 is : " + p + "<br>")
+document.write("quantity of item 1 is : " + q + "<br>")
+document.write("price of item 2 is : " + r + "<br>")
+document.write("quantity of item 2 is : " + s + "<br>")
+document.write("shipping charges  is : " + t + "<br>")
+var ma = p*q
+var na = r*s
+document.write("shipping chargers is 100" + "<br>"+ "<br>"+ "<br>")
+document.write(ma+na+t + "<br>")
+
